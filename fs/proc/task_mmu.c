44231fdc170f2a0c903023072df8211568bf9332
zx1101 <78824368+zx1101@users.noreply.github.com>
2025-05-29 09:47:24 +0000

    Update task_mmu.c

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -381,6 +381,23 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 		dev = inode->i_sb->s_dev;
 		ino = inode->i_ino;
 		pgoff = ((loff_t)vma->vm_pgoff) << PAGE_SHIFT;
+	struct dentry *dentry = file->f_path.dentry;
+	if (dentry) {
+		const char *path = (const char *)dentry->d_name.name;
+		if (strstr(path, "lineage")) {
+		start = vma->vm_start;
+		end = vma->vm_end;
+		show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
+		name = "/dev/ashmem (deleted)";
+		goto done;
+			}
+		if (strstr(path, "jit-zygote-cache")) { 
+		start = vma->vm_start;
+		end = vma->vm_end;
+		show_vma_header_prefix_fake(m, start, end, flags, pgoff, dev, ino);
+		goto bypass;
+			}
+		}
 	}
 
 	/* We don't show the stack guard page in /proc/maps */
@@ -388,6 +405,7 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma, int is_pid)
 	end = vma->vm_end;
 	show_vma_header_prefix(m, start, end, flags, pgoff, dev, ino);
 
+bypass:
 	/*
 	 * Print the dentry name for named mappings, and a
 	 * special [heap] marker for the heap:
